579944aae4c6e7d51a03ef5a6163d61e28f41131
Jaime Ilustre <jaimeilustre@yahoo.com>
2024-03-20 15:34:24 +0100

    added more details to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,15 +6,29 @@ function HomePage() {
 const { journeys, getJourneys } = useContext(JourneyContext)
 console.log(journeys)
 
+const dateString = journeys[0].departureTime
+console.log(dateString)
+
     return(
         <>
             <h1>Homepage</h1>
 
-            {journeys.map(journey => (
-                <div key={journey._id}>
-                    <h2>{journey.trainNumber}</h2>
-                </div>
-            ))}
+            {journeys.length !== 0 ? (
+                journeys.map(journey => (
+                    <div key={journey._id}>
+                        <h2>{journey.trainNumber}</h2>
+                        <h3>{journey.departureTime}</h3>
+                        <h3>{journey.departureStation}</h3>
+                        <h3>{journey.arrivalStation}</h3>
+                        <h3>{journey.duration}</h3>
+                        <h3>{journey.trainType}</h3>
+                        <h3>{journey.numberOfSeats}</h3>
+                        <h3>{journey.price}</h3>
+                    </div>
+                ))
+            ) : (
+                <h1>Loading...</h1>
+            )}
         </>
     )
 }
